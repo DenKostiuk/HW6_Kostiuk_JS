1dd2d7af92a85d4e1e78cc77dbed84fb98f33650
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-12-21 22:15:39 +0200

    3

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 let sum1 = ([]); // 0
 let sum2 = ([1, 2, 3]); // 6
 let sum3 = ([5, 0, 10, 1, 5, null, 'some string']); // 21
-
+let sum4 = ([0, 7, 10, NaN, 1, 5, 'some string']); 
+let sum5 = ([5, 10, 10, Infinity, 1, 5, undefined, 'some string']); 
 
 function calcSum(array) {
     let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (isNaN(array[i]) == true) { 
-            continue
+    array.forEach(function calcSum(currentValue) {
+        if (isFinite(currentValue)) {
+            sum += currentValue;
         }
-        else { 
-            sum += Number(array[i]) 
-        }
-    }
-    console.log(sum);
+    });
+
+    return sum;
 }
-calcSum(sum1);
-calcSum(sum2);
-calcSum(sum3);
+
+console.log(calcSum(sum1));
+console.log(calcSum(sum2));
+console.log(calcSum(sum3));
+console.log(calcSum(sum4));
+console.log(calcSum(sum5));
