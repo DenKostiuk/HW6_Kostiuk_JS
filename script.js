8cdec8a89d0b70b21541e12dc18fb6dce995edfb
Denys Kostiuk <93026512+DenKostiuk@users.noreply.github.com>
2021-12-21 21:53:19 +0200

    3

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let array = [1, 2, 3, 4, 5];
+let sum1 = ([]); // 0
+let sum2 = ([1, 2, 3]); // 6
+let sum3 = ([5, 0, 10, 1, 5, null, 'some string']); // 21
 
-function changeFour(array) {
-    let newArray = array.slice();
-    newArray[3] = 'JavaScript';
-    return newArray;
+
+function calcSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (isNaN(array[i]) == true) { 
+            continue
+        }
+        else { 
+            sum += Number(array[i]) 
+        }
+    }
+    console.log(sum);
 }
-
-let result = changeFour(array);
-
-console.log(array); // [1, 2, 3, 4, 5];
-console.log(result); // [1, 2, 3, 'JavaScript', 5];
+calcSum(sum1);
+calcSum(sum2);
+calcSum(sum3);
